d4b04a39156faaaaa27335ac83fe3dbd09669f50
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 20:27:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   end
 
   def artist_name=(artist_name)
-      @artist = Artist.find_or_create_by_name(artist_name)
+      artist = Artist.find_or_create_by_name(artist_name)
       artist.add_song(self)
   end
 
