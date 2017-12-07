f8010f5c93a7c6841fea6001a9d90c298b3e011f
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 20:32:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,9 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    # binding.pry
     artist_name = filename.split(" - ")[0]
     title = filename.split(" - ")[1]
     new_song = self.new(title)
-    #binding.pry
     new_song.artist_name = artist_name
 
     new_song
