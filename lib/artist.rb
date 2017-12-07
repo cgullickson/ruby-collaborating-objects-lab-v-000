f63ebbda05a6ea68d35907c37a7af53136ad181e
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 19:20:40 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+class Artist
+  attr_accessor :name, :genre, :songs, :song
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @genre = genre
+    @songs = []
+  end
+
+  def add_song (song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find (name)
+    @@all.detect {|artist| artist.name == name}
+  end
+
+  def self.create (name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
+
+  def self.find_or_create_by_name (artist)
+    self.find(artist) || self.create(artist)
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize (name)
+    @name = name
+    @artist = artist
+  end
+
+  def artist_name=(name)
+      @artist = Artist.find_or_create_by_name(name)
+      artist.add_song.self
+  end
+
+  def self.new_by_filename(filename)
+    title = filename.split(" - ")[1]
+    artist = filename.split(" - ")[2]
+    newsong = self.new(title)
+    newsong.artist_name = artist_name
+    newsong
+  end
+
+end
