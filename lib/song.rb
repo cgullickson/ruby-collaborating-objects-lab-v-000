0e661b7658e0d2aea4a044037152eb2b2271fd59
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 20:02:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     title = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[2]
     new_song = self.new(title)
-    new_song.artist_name= artist_name
+    new_song.artist_name = artist_name
     new_song
   end
 
